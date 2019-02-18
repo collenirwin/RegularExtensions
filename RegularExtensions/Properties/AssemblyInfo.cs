9da3ab1419aba38dd35c4fbb93794bb5ae531894
collenirwin <collenirwin@gmail.com>
2019-02-17 22:54:05 -0500

    add nuget package

--- RegularExtensions/Properties/AssemblyInfo.cs
+++ RegularExtensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RegularExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Regular Extensions")]
+[assembly: AssemblyDescription("Regular expression extension methods for string")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Collen Irwin")]
-[assembly: AssemblyProduct("RegularExtensions")]
+[assembly: AssemblyProduct("Regular Extensions")]
 [assembly: AssemblyCopyright("Copyright © Collen Irwin 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
